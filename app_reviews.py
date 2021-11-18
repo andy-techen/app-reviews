63f729192829df9684123a6c9c0229adf9c89bb6
Andy <andy27479@gmail.com>
2021-11-18 15:54:23 -0500

    added __name__ == __main__, reformulated input form

--- app_reviews.py
+++ app_reviews.py
@@ -5,20 +5,19 @@ from nltk.corpus import stopwords
 from google_play_scraper import Sort, reviews as reviews_par
 from app_store_scraper import AppStore
 from sklearn.feature_extraction.text import TfidfTransformer, CountVectorizer
-import jieba
-import jieba.analyse
+# import jieba
+# import jieba.analyse
 
 # prelims
 stop = stopwords.words('english') + list(string.punctuation)
 transformer = TfidfTransformer()
 cv = CountVectorizer()
-jieba.enable_paddle()
+# jieba.enable_paddle()
 
-# piccollage: 'com.cardinalblue.piccollage.google'
 def get_play_store(app_id, n_reviews=200, filter="all"):
     """get play store reviews for app"""
     reviews, _ = reviews_par(
-        app_id, count=n_reviews, lang='zh', country='tw',
+        app_id, count=n_reviews, lang='en', country='us',
         sort=Sort.MOST_RELEVANT)
     reviews_df = pd.DataFrame(reviews)
     if filter == "all":
@@ -26,10 +25,9 @@ def get_play_store(app_id, n_reviews=200, filter="all"):
     else:
         return reviews_df.query('score' + filter)
 
-# piccollage: 'piccollage-photo-grid-editor'
 def get_app_store(app_id, n_reviews=200, filter="all"):
     """get play store reviews for app"""
-    data = AppStore(app_name=app_id, country='tw')
+    data = AppStore(app_name=app_id, country='us')
     data.review(how_many=n_reviews)
     reviews = data.reviews
     reviews_df = pd.DataFrame(reviews)
@@ -44,7 +42,8 @@ def get_features(contents, n_features=50, lang='en'):
     for content in contents:
         try:
             if lang == 'zh':
-                text_ls = [*jieba.cut(content, use_paddle=True)]
+                # text_ls = [*jieba.cut(content, use_paddle=True)]
+                pass
             else:
                 text_ls = word_tokenize(content)
                 text_ls = [txt.lower() for txt in text_ls if txt.lower() not in stop]
@@ -64,11 +63,26 @@ def get_features(contents, n_features=50, lang='en'):
 
     return tfidf_df, features_all.nlargest(n_features)
 
-# get features of negative (ratings <= 3) reviews
-play_store = get_play_store('com.cardinalblue.piccollage.google', 10000, '<=3')
-app_store = get_app_store('piccollage-photo-grid-editor', 10000, '<=3')
+if __name__ == '__main__':
+    app_store_name = input("Enter iOS app name (https://apps.apple.com/us/app/<app_name>/<app_id>): ")
+    play_store_name = input("Enter Android app name (https://play.google.com/store/apps/details?id=<app_name>&hl=en_US&gl=US): ")
+    n_reviews = int(input("Enter number of reviews: "))
+    
+    if app_store_name != "" and play_store_name != "":
+        app_store = get_app_store(app_store_name, n_reviews, '<=3')
+        play_store = get_play_store(play_store_name, n_reviews, '<=3')
+        reviews = pd.concat([play_store['content'], app_store['review']]).tolist()
 
-all_reviews = pd.concat([play_store['content'], app_store['review']]).tolist()
-all_reviews = [x for x in all_reviews if x]  # remove empty strings
+    elif app_store_name == "":
+        play_store = get_play_store(play_store_name, n_reviews, '<=3')
+        reviews = play_store['content'].tolist()
+    
+    elif play_store_name == "":
+        app_store = get_app_store(app_store_name, n_reviews, '<=3')
+        reviews = app_store['review'].tolist()
+    
+    else:
+        print("Enter app's name on App Store or Play Store.")
 
-features_matrix, top_features = get_features(all_reviews, lang='zh')
+    features_matrix, top_features = get_features(reviews, lang='en')
+    top_features.to_csv(f'{app_store_name}.csv', header=False)
